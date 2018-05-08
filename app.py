99b0f3bb2a6a34253e95496ef812a2adb24a4375
unknown <kiokoleon@gmail.com>
2018-05-08 15:21:54 +0300

    Update specify port

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def add():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run('', port=port)
+    app.run(host='0.0.0.0', port=port)
     # app.run() # run locally
